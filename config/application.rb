00e191bb68c0992d5b71f58948cbd11917f8202d
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-06-13 10:13:34 +0000

    Setup asset_host, and assets prefix

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module ContactsFrontend
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.prefix = 'contacts-frontend'
+
     # Disable rack::cache
     config.action_dispatch.rack_cache = nil
   end
