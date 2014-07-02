3dbec715346d02251241b00b968784b1ba23ff89
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-07-02 14:30:09 +0000

    Support contact pages at new URL.
    Contact pages are going to be served from
    /government/organisations/<org_slug>/contact/<contact_slug> instead of
    the original url of /contact/<org_slug>/<contact_slug>.  This change
    makes this app support both schemes.  The old one will be removed once
    everything has switched over.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   with_options :format => false do |routes|
-    routes.get "/#{APP_SLUG}/:organisation/:id" => "contacts#show"
+    routes.get "/government/organisations/:organisation/contact/:id" => "contacts#show"
+
+    # FIXME: Remove this route once it's no longer being used by anything
+    routes.get "/contact/:organisation/:id" => "contacts#show"
+
     routes.get "/healthcheck" => proc {|env| [200, {}, ["OK"]] }
   end
 end
--- spec/features/contact_page_spec.rb
+++ spec/features/contact_page_spec.rb
@@ -1,10 +1,31 @@
 require 'spec_helper'
 
 feature "Showing a contact page" do
+
   it "renders a contact page" do
-    path = '/contact/hm-revenue-customs/annual-tax-on-enveloped-dwellings-ated'
+    path = '/government/organisations/hm-revenue-customs/contact/annual-tax-on-enveloped-dwellings-ated'
 
     content_store_has_item(path, read_content_store_fixture('hmrc_ated'))
+
+    visit(path)
+
+    expect(page).to have_content("Annual Tax on Enveloped Dwellings")
+    expect(page.response_headers["Cache-Control"]).to eq("max-age=1800, public")
+  end
+
+  it "should 404 for a non-existent item in the content-store" do
+    path = '/government/organisations/hm-revenue-customs/contact/bear-tax'
+
+    content_store_does_not_have_item(path)
+
+    visit(path)
+    expect(page.status_code).to eq(404)
+  end
+
+  it "renders a contact page at the old URL" do
+    path = '/contact/hm-revenue-customs/annual-tax-on-enveloped-dwellings-ated'
+
+    content_store_has_item(path, read_content_store_fixture('hmrc_ated_legacy_url'))
 
     visit(path)
 
