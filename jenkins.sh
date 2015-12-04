ef45cff28681e88748c854a713e42783693e4c8c
David Basalla <davidbasalla@gmail.com>
2015-12-04 11:22:47 +0000

    Add govuk-content-schemas-test-helpers gem and Jenkins config
    - This needed the "addressable" gem to be updated, as well as config
      adding to make CI able to find `govuk-content-schemas`, as per the
      `jenkins.sh` in contacts-admin.

--- jenkins.sh
+++ jenkins.sh
@@ -6,4 +6,8 @@ export RAILS_ENV=test
 git clean -fdx
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 
-bundle exec rake
+# Clone govuk-content-schemas depedency for contract tests
+rm -rf tmp/govuk-content-schemas
+git clone git@github.com:alphagov/govuk-content-schemas.git tmp/govuk-content-schemas
+
+GOVUK_CONTENT_SCHEMAS_PATH=tmp/govuk-content-schemas COVERAGE=on bundle exec rake
