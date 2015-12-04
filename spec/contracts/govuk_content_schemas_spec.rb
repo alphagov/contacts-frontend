1fd48cd769526bc058fe23bf3b4c9128646621a5
David Basalla <davidbasalla@gmail.com>
2015-12-04 11:24:46 +0000

    Add contract tests for contact content schema
    Inspired by documentation for govuk-content-schema-helpers (https://github.com/alphagov/govuk-content-schema-test-helpers/blob/master/README.md).
    File structure similar to implementation of contract testing for `manuals-frontend`

---
+++ spec/contracts/govuk_content_schemas_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+require "gds_api/test_helpers/content_store"
+
+RSpec.describe "Schema compatibility", type: :request do
+  include GdsApi::TestHelpers::ContentStore
+
+  all_examples_for_supported_formats = GovukContentSchemaTestHelpers::Examples.new.get_all_for_formats(%w{
+    contact
+  })
+  
+  all_examples_for_supported_formats.each do |example|
+    content_item = JSON.parse(example)
+
+    it "can handle a request for #{content_item["base_path"]}" do
+      content_store_has_item(content_item['base_path'], content_item)
+
+      get content_item['base_path']
+      expect(response.status).to eq(200)
+      assert_select 'title', Regexp.new(Regexp.escape(content_item['title']))
+    end
+  end
+end
